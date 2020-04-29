1766e9641e899859610746f898bf04c5fed9c03a
timush-a <smtimush@yandex.ru>
2020-04-29 22:27:14 +0300

    Update exchange_rates.py

--- exchange_rates.py
+++ exchange_rates.py
@@ -30,5 +30,23 @@ def get_bm_exchange_rates():
         'bm_euro_selling_rate' : currency_list[3]
         }
 
-    with open('exchange.txt', 'w') as file:
-        file.write(bm_exchange_rates)
+    with open('mb_exchange.txt', 'w') as file:
+        file.write(str(bm_exchange_rates))
+
+def get_rncb_exchange_rates():
+    rncb_currency_page = bs(rncb_req, 'lxml')
+    raw_currency_list = rncb_currency_page.findChild('table', {'class': 'cours'}).find_all('td')
+    currency_list = re.findall(r"\d+.\d?", str(raw_currency_list))
+    rncb_exchange_rates = {
+        'rncb_dollar_purchase_rate' : currency_list[0],
+        'rncb_dollar_selling_rate' : currency_list[1],
+        'rncb_euro_purchase_rate' : currency_list[2],
+        'rncb_euro_selling_rate' : currency_list[3]
+        }
+    with open('rncb_exchange.txt', 'w') as file:
+        file.write(str(rncb_exchange_rates))
+
+
+if __name__ == '__main__':
+    get_rncb_exchange_rates()
+    get_bm_exchange_rates()
