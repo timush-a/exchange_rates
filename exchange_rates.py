43fc76c1cace4eb7b1417f749183d667c8d20636
timush-a <smtimush@yandex.ru>
2020-05-02 00:11:40 +0300

    Add description

--- exchange_rates.py
+++ exchange_rates.py
@@ -3,6 +3,12 @@ import re
 import requests
 from bs4 import BeautifulSoup as bs
 
+"""
+My first try to use web scraping with Python.
+I'm currently interested in, where to buy dollars, or euro
+with maximum profit.
+"""
+
 bm_url = "http://morskoybank.com"
 rncb_url = "https://www.rncb.ru/fizicheskkim-litsam/valyutnye-operatsii/"
 folder = 'exchange'
@@ -23,6 +29,7 @@ def get_bm_exchange_rates():
     bm_currency_page = bs(bm_req, 'lxml')
     raw_currency_list = bm_currency_page.findChild('table', {'class': 'little'}).find_all('td')
     currency_list = re.findall(r"\d+,\d+", str(raw_currency_list))
+
     bm_exchange_rates = {
         'bm_dollar_purchase_rate' : currency_list[0],
         'bm_dollar_selling_rate' : currency_list[1],
@@ -32,21 +39,34 @@ def get_bm_exchange_rates():
 
     with open('mb_exchange.txt', 'w') as file:
         file.write(str(bm_exchange_rates))
+    return bm_exchange_rates
 
 def get_rncb_exchange_rates():
     rncb_currency_page = bs(rncb_req, 'lxml')
     raw_currency_list = rncb_currency_page.findChild('table', {'class': 'cours'}).find_all('td')
     currency_list = re.findall(r"\d+.\d?", str(raw_currency_list))
+
     rncb_exchange_rates = {
         'rncb_dollar_purchase_rate' : currency_list[0],
         'rncb_dollar_selling_rate' : currency_list[1],
         'rncb_euro_purchase_rate' : currency_list[2],
         'rncb_euro_selling_rate' : currency_list[3]
         }
+
     with open('rncb_exchange.txt', 'w') as file:
         file.write(str(rncb_exchange_rates))
+    return rncb_exchange_rates
+
+
+def where_to_buy_currency():
+    pass
+     
+
+def where_to_sell_currency():
+    pass    
 
 
 if __name__ == '__main__':
-    get_rncb_exchange_rates()
-    get_bm_exchange_rates()
+    pass
+    
+    
