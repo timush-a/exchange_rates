d4b8ebd51afd5ede4f4132156ce6bcc8b4463ff3
timush-a <smtimush@yandex.ru>
2020-05-03 23:19:22 +0300

    Added two function's where_to_buy_dollars, where_to_sell_dollars
    Added two function's which calculates where it is better to buy, or to sell dollars

--- exchange_rates.py
+++ exchange_rates.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -14,7 +15,6 @@ rncb_url = "https://www.rncb.ru/fizicheskkim-litsam/valyutnye-operatsii/"
 folder = 'exchange'
 bm_req = requests.get(bm_url).text
 rncb_req = requests.get(rncb_url).text
-pattern = r"\d+,\d+"
 
 
 try:
@@ -24,50 +24,70 @@ except OSError:
 finally:
     os.chdir(folder)
 
+def exact_time():
+        return time.strftime('%Y-%m-%d__%H-%m-%S', time.localtime())
 
-class GetBmExchangeRates():
+class BmExchangeRates():
     bm_currency_page = bs(bm_req, 'lxml')
     raw_currency_list = bm_currency_page.findChild('table', {'class': 'little'}).find_all('td')
     currency_list = re.findall(r"\d+,\d+", str(raw_currency_list))
 
-    bm_exchange_rates = {
-        'bm_dollar_purchase_rate' : float(currency_list[0].replace(',', '.')),
-        'bm_dollar_selling_rate' : float(currency_list[1].replace(',', '.')),
-        'bm_euro_purchase_rate' : float(currency_list[2].replace(',', '.')),
-        'bm_euro_selling_rate' : float(currency_list[3].replace(',', '.'))
+    bm_rates = {
+        'dollars_purchase' : float(currency_list[0].replace(',', '.')),
+        'dollars_selling' : float(currency_list[1].replace(',', '.'))
         }
 
-    with open('mb_exchange.txt', 'w') as file:
+       
+    with open(f'bm_rates_{exact_time()}.txt', 'w') as file:
         file.write(str(bm_exchange_rates))
 
 
-class GetRncbExchangeRates():
+class RncbExchangeRates():
     rncb_currency_page = bs(rncb_req, 'lxml')
     raw_currency_list = rncb_currency_page.findChild('table', {'class': 'cours'}).find_all('td')
-    currency_list = re.findall(r"(\d{2}.\d{1,2})", str(raw_currency_list))
+    currency_list = re.findall(r"(\d{2}.\d{1,2})", str(raw_currency_list))  # this pattern will be used to find numbers of the form like 99.99
 
-    rncb_exchange_rates = {
-        'rncb_dollar_purchase_rate' : currency_list[0],
-        'rncb_dollar_selling_rate' : currency_list[1],
-        'rncb_euro_purchase_rate' : currency_list[2],
-        'rncb_euro_selling_rate' : currency_list[3]
+    rncb_rates = {
+        'dollars_purchase' : float(currency_list[0]),
+        'dollars_selling' : float(currency_list[1]),
         }
+    
     def display(self, info=rncb_exchange_rates):
         print(info)
     
-    with open('rncb_exchange.txt', 'w') as file:
+    with open(f'rncb_rates_{exact_time()}.txt', 'w') as file:
         file.write(str(rncb_exchange_rates))
 
 
-def where_to_buy_currency():
-    pass
+def where_to_sell_dollars():
+    
+    BmExchangeRates()
+    RncbExchangeRates()
+    
+    if rncb_rates[dollars_purchase] >= bm_rates[dollars_purchase]:
+        difference= rncb_rates[dollars_purchase] - bm_rates[dollars_purchase]
+        print('You should sell dollars in RNCB\n')
+        print(f'Difference of rates is equal {difference})
+    else:
+              difference = bm_rates[dollars_purchase] - rncb_rates[dollars_purchase]
+              print('You should sell dollars in Sea Bank\n')
+              print(f'Difference of rates is equal {differnce_of_rates})
      
 
-def where_to_sell_currency():
-    pass    
+def where_to_buy_dollars():
+    BmExchangeRates()
+    RncbExchangeRates()
+    
+    if rncb_exchange_rates[1] >= bm_exchange_rates[1]:
+        difference_of_rates = rncb_exchange_rates[0] - bm_exchange_rates[0]
+        print('You should sell dollars in RNCB\n')
+        print(f'Difference of rates is equal {differnce_of_rates})
+    else:
+              difference_of_rates = bm_exchange_rates[0] - rncb_exchange_rates[0]
+              print('You should sell dollars in Sea Bank\n')
+              print(f'Difference of rates is equal {differnce_of_rates})   
 
-may_1 = GetRncbExchangeRates()
-may_1.display()
+
 
     
     
