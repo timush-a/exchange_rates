038a6057892729e352b4586fe14685c2446df0cf
timush-a <smtimush@yandex.ru>
2020-05-03 23:23:24 +0300

    Update according to pep8

--- exchange_rates.py
+++ exchange_rates.py
@@ -24,8 +24,10 @@ except OSError:
 finally:
     os.chdir(folder)
 
+
 def exact_time():
         return time.strftime('%Y-%m-%d__%H-%m-%S', time.localtime())
+
 
 class BmExchangeRates():
     bm_currency_page = bs(bm_req, 'lxml')
@@ -33,11 +35,10 @@ class BmExchangeRates():
     currency_list = re.findall(r"\d+,\d+", str(raw_currency_list))
 
     bm_rates = {
-        'dollars_purchase' : float(currency_list[0].replace(',', '.')),
-        'dollars_selling' : float(currency_list[1].replace(',', '.'))
+        'dollars_purchase': float(currency_list[0].replace(',', '.')),
+        'dollars_selling': float(currency_list[1].replace(',', '.'))
         }
 
-       
     with open(f'bm_rates_{exact_time()}.txt', 'w') as file:
         file.write(str(bm_exchange_rates))
 
@@ -48,46 +49,42 @@ class RncbExchangeRates():
     currency_list = re.findall(r"(\d{2}.\d{1,2})", str(raw_currency_list))  # this pattern will be used to find numbers of the form like 99.99
 
     rncb_rates = {
-        'dollars_purchase' : float(currency_list[0]),
-        'dollars_selling' : float(currency_list[1]),
+        'dollars_purchase': float(currency_list[0]),
+        'dollars_selling': float(currency_list[1]),
         }
-    
+
     def display(self, info=rncb_exchange_rates):
         print(info)
-    
+
     with open(f'rncb_rates_{exact_time()}.txt', 'w') as file:
         file.write(str(rncb_exchange_rates))
 
 
 def where_to_sell_dollars():
-    
+
     BmExchangeRates()
     RncbExchangeRates()
-    
+
     if rncb_rates[dollars_purchase] >= bm_rates[dollars_purchase]:
-        difference= rncb_rates[dollars_purchase] - bm_rates[dollars_purchase]
+        difference = rncb_rates[dollars_purchase] - bm_rates[dollars_purchase]
         print('You should sell dollars in RNCB\n')
         print(f'Difference of rates is equal {difference})
     else:
-              difference = bm_rates[dollars_purchase] - rncb_rates[dollars_purchase]
-              print('You should sell dollars in Sea Bank\n')
-              print(f'Difference of rates is equal {differnce_of_rates})
-     
+        difference = bm_rates[dollars_purchase] - rncb_rates[dollars_purchase]
+        print('You should sell dollars in Sea Bank\n')
+        print(f'Difference of rates is equal {differnce_of_rates})
+
 
 def where_to_buy_dollars():
     BmExchangeRates()
     RncbExchangeRates()
-    
+
     if rncb_exchange_rates[1] >= bm_exchange_rates[1]:
         difference_of_rates = rncb_exchange_rates[0] - bm_exchange_rates[0]
         print('You should sell dollars in RNCB\n')
         print(f'Difference of rates is equal {differnce_of_rates})
     else:
-              difference_of_rates = bm_exchange_rates[0] - rncb_exchange_rates[0]
-              print('You should sell dollars in Sea Bank\n')
-              print(f'Difference of rates is equal {differnce_of_rates})   
+        difference_of_rates = bm_exchange_rates[0] - rncb_exchange_rates[0]
+        print('You should sell dollars in Sea Bank\n')
+        print(f'Difference of rates is equal {differnce_of_rates})
 
-
-
-    
-    
