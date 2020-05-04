549dc8d844426a29388c6dc6cd4d09a036edec29
timush-a <smtimush@yandex.ru>
2020-05-05 00:24:36 +0300

    added message output

--- exchange_rates.py
+++ exchange_rates.py
@@ -21,12 +21,12 @@ class BmExchangeRates():
         raw_currency_list = currencies_page.findChild('table', {'class': 'little'}).find_all('td')
         currency_list = re.findall(r"(\d{2}.\d{1,2})", str(raw_currency_list))  # this pattern will be used to find numbers of the form like 99.99
 
-        rates = {
+        self.rates = {
             'dollars_purchase': float(currency_list[0].replace(',', '.')),
             'dollars_selling': float(currency_list[1].replace(',', '.'))
             }
 
-    def __str__(self, rates):
+    def __repr__(self):
         return rates
 
     def exact_time():
@@ -41,19 +41,15 @@ class RncbExchangeRates(BmExchangeRates):
     rncb_url = "https://www.rncb.ru/fizicheskkim-litsam/valyutnye-operatsii/"
 
     def __init__(self, url=rncb_url):
-        request = requests.get(url).text
-
+        request = requests.get(url).text  
         currencies_page = bs(request, 'lxml')
         raw_currency_list = currencies_page.findChild('table', {'class': 'cours'}).find_all('td')
         currency_list = re.findall(r"(\d{2}.\d{1,2})", str(raw_currency_list))
 
-        rates = {
+        self.rates = {
             'dollars_purchase': float(currency_list[0]),
             'dollars_selling': float(currency_list[1]),
             }
-
-    def display(self):
-        print(rates)
 
     def save_rates(name=f'rncb_rates_{BmExchangeRates.exact_time()}.txt'):
         with open('name.txt', 'w') as file:
@@ -61,36 +57,33 @@ class RncbExchangeRates(BmExchangeRates):
 
 
 class BuyingAndSelling():
-
-    bm = BmExchangeRates()
-    rncb = RncbExchangeRates()
-
-    def where_to_sell_dollars(self):
+    def __init__(self):
+        bm = BmExchangeRates()
+        rncb = RncbExchangeRates()
+        self.buy_difference = abs(rncb.rates['dollars_selling'] - bm.rates['dollars_selling'])
+        self.sell_difference = abs(rncb.rates['dollars_purchase'] - bm.rates['dollars_purchase'])
+        if rncb.rates['dollars_selling'] <= bm.rates['dollars_selling']:
+            print((
+                "Now it’s better to buy dollars at the RNCB \n"
+                f"Dollar rate in RNCB = {rncb.rates['dollars_selling']} \n"
+                f"Dollar rate in BM = {bm.rates['dollars_selling']} \n"
+                f"Difference between the dollar rates in this banks = {self.buy_difference}\n\n"))      
+        else:
+            print((
+                "Now it’s better to buy dollars at the BM \n"
+                f"Dollar rate in RNCB = {rncb.rates['dollars_selling']} \n"
+                f"Dollar rate in BM = {bm.rates['dollars_selling']} \n"
+                f"Difference between the dollar rates in this banks = {self.buy_difference}\n\n"))
+            
         if rncb.rates['dollars_purchase'] >= bm.rates['dollars_purchase']:
-            difference = rncb.rates['dollars_purchase'] - bm.rates['dollars_purchase']
-            print('You should to sell dollars in RNCB')
-            print(f'Difference of rates is equal {difference}')
+            print((
+                "Now it’s better to sell dollars at the RNCB \n"
+                f"Dollar rate in RNCB = {rncb.rates['dollars_purchase']} \n"
+                f"Dollar rate in BM = {bm.rates['dollars_purchase']} \n"
+                f"Difference between the dollar rates in this banks = {self.sell_difference}"))     
         else:
-            difference = bm.rates['dollars_purchase'] - rncb.rates['dollars_purchase']
-            print('You should to sell dollars in Sea Bank')
-            print(f'Difference of rates is equal {difference}')
-
-    def where_to_buy_dollars(self):
-        if rncb.rates['dollars_selling'] >= bm.rates['dollars_selling']:
-            difference = rncb.rates['dollars_selling'] - bm.rates['dollars_selling']
-            print(
-                f"""You should to buy dollars in RNCB /n
-                Dollar rate in a RNCB ---- {rncb.rates['dollars_selling']} /n
-                Dollar rate in a BM ---- {bm.rates['dollars_selling']} /n
-                Difference between the dollar rates in this banks is equal {difference}"""
-                )
-        
-        else:
-            difference = bm.rates['dollars_selling'] - rncb.rates['dollars_selling']
-            print(
-                f"""You should to buy dollars in RNCB /n
-                Dollar rate in a RNCB ---- {rncb.rates['dollars_selling']} /n
-                Dollar rate in a BM ---- {bm.rates['dollars_selling']} /n
-                Difference between the dollar rates in this banks is equal {difference}"""
-                )
-        
+            print((
+                "Now it’s better to sell dollars at the BM \n"
+                f"Dollar rate in RNCB = {rncb.rates['dollars_purchase']} \n"
+                f"Dollar rate in BM = {bm.rates['dollars_purchase']} \n"
+                f"Difference between the dollar rates in this banks = {self.sell_difference}"))
